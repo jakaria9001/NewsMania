59902ab520ab2094a541c452dc5e88e015e0acea
Jakaria Hussain <jakariahussain9001@gmail.com>
2021-11-03 21:26:42 +0530

    Resolved error that occured during deploy

--- routes/news.js
+++ routes/news.js
@@ -6,33 +6,10 @@ router.get('/latest', async(req,res)=>{ // shows the news registered ordered by 
     try {
         
         console.log("OK");
-        res.json({msg: "Lates coming"});
+        res.json({msg: "Latest news loading..."});
     } catch (error) {
         // next(error);
     }
 })
 
-// router.get('/specific/:id', async(req,res, next)=>{ // shows the complaints registered by all users)
-//     try {
-//         const complaint = await Complaint.find({})
-//         if(!complaint){ 
-//             return res.status(400).json({
-//                 success: false,
-//                 msg: "Something went wrong"
-//             })
-//         }
-// 		const total = await Complaint.estimatedDocumentCount();
-// 		res.set("x-total-count",total);
-		
-//         res.status(200).json({
-//             success: true,
-//             count: complaint.length,
-//             complaints: complaint,
-//             msg: "All complaints fetced"
-//         })
-
-//     } catch (error) {
-//         next(error);
-//     }
-// })
 module.exports = router;
